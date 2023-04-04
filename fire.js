c9d3ada403a0877417d3783a153a79d832ed0a69
Sharduli  Pandey <shardulipandey8090@gmail.com>
2023-04-04 18:22:21 +0530

    Updated Security

--- fire.js
+++ fire.js
@@ -1,23 +1,8 @@
 
-            // Import the functions you need from the SDKs you need
+            
             import {initializeApp} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
-            // TODO: Add SDKs for Firebase products that you want to use
-            // https://firebase.google.com/docs/web/setup#available-libraries
-
-            // Your web app's Firebase configuration
-            // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-            const firebaseConfig = {
-                apiKey: "AIzaSyDSdF8WsvzvAfPdmX38q6rTdztm_ldbjqc",
-                authDomain: "portfolio-5eb79.firebaseapp.com",
-                projectId: "portfolio-5eb79",
-                storageBucket: "portfolio-5eb79.appspot.com",
-                messagingSenderId: "555966021794",
-                appId: "1:555966021794:web:b6295704e59cf1b743d7fb",
-                measurementId: "G-EZ6B5Z1RY8"
-            };
-            // Import the functions you need from the SDKs you need
-            // TODO: Add SDKs for Firebase products that you want to use
-            // https://firebase.google.com/docs/web/setup#available-libraries
+            
+            import {firebaseConfig} from "./fireCreds.js";
 
             import {
                 getFirestore,
---
+++ fireCreds.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyDSdF8WsvzvAfPdmX38q6rTdztm_ldbjqc",
+    authDomain: "portfolio-5eb79.firebaseapp.com",
+    projectId: "portfolio-5eb79",
+    storageBucket: "portfolio-5eb79.appspot.com",
+    messagingSenderId: "555966021794",
+    appId: "1:555966021794:web:b6295704e59cf1b743d7fb",
+    measurementId: "G-EZ6B5Z1RY8"
+};
