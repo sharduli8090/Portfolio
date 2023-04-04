3245eaf495bc8df30297255cfc84571b658ced7e
Sharduli  Pandey <shardulipandey8090@gmail.com>
2023-04-04 18:43:38 +0530

    Updated the Errors

--- fire.js
+++ fire.js
@@ -2,7 +2,15 @@
             
             import {initializeApp} from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
             
-            import {firebaseConfig} from "./fireCreds.js";
+          const firebaseConfig = {
+                apiKey: "AIzaSyDSdF8WsvzvAfPdmX38q6rTdztm_ldbjqc",
+                authDomain: "portfolio-5eb79.firebaseapp.com",
+                projectId: "portfolio-5eb79",
+                storageBucket: "portfolio-5eb79.appspot.com",
+                messagingSenderId: "555966021794",
+                appId: "1:555966021794:web:b6295704e59cf1b743d7fb",
+                measurementId: "G-EZ6B5Z1RY8"
+            };
 
             import {
                 getFirestore,
